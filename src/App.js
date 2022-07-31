ba2444e6a459362a671731deab773ce0a52630c2
Erik <Mr.erik.egliens@gmail.com>
2022-07-31 21:15:01 +0300

    words count func

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ function App() {
   const [typeData, setTypeData] = React.useState({
     textareaValue: "",
   })
+
+  const [wordsNum, setWordsNum] = React.useState(0)
+
+console.log(wordsNum);
+  
+  function countWords(str) {
+    const arr = str.trim().split(' ');
+    return arr.filter(word => word !== "").length
+  }
+
+
+
+  function handleClick() {
+    console.log(123);
+    setWordsNum(countWords(typeData.textareaValue))
+    
+  }
+
   function handleChange(event) {
     const {name, value} = event.target
     setTypeData(prevData => {
@@ -15,11 +33,11 @@ function App() {
   }
   return (
       <>
-        <h1>title</h1>
+        <h1>Title</h1>
         <textarea name="textareaValue" onChange={handleChange} value={typeData.textareaValue}/>
-        <h4>the amount of time remaining</h4>
-        <button>start the game</button>
-        <h1>word count</h1>
+        <h4>The amount of time remaining</h4>
+        <button onClick={handleClick}>Start the game</button>
+        <h1>{wordsNum}</h1>
       </>
   )
 }
