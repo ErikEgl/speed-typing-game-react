c812c953a3d13ecb4ad5ab460b54ca086a52ad81
Erik <Mr.erik.egliens@gmail.com>
2022-07-31 21:14:18 +0300

    typing data gathering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 function App() {
+  const [typeData, setTypeData] = React.useState({
+    textareaValue: "",
+  })
+  function handleChange(event) {
+    const {name, value} = event.target
+    setTypeData(prevData => {
+      return {
+        ...prevData,
+        [name]: value
+      }
+    })
+  }
   return (
       <>
         <h1>title</h1>
-        <textarea value="123"/>
+        <textarea name="textareaValue" onChange={handleChange} value={typeData.textareaValue}/>
         <h4>the amount of time remaining</h4>
         <button>start the game</button>
         <h1>word count</h1>
