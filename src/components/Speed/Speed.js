48fd75b1b3db12fe527bc877816a31e59dd64d02
Erik <Mr.erik.egliens@gmail.com>
2022-11-14 14:24:56 +0200

    changed variable

--- src/components/Speed/Speed.js
+++ src/components/Speed/Speed.js
@@ -2,9 +2,9 @@ import { useContext } from "react";
 import { UserContext } from "../../useContext";
 
 function Speed() {
-    const { wordsNum, startingTime } = useContext(UserContext);
-    if(wordsNum !== 0 && startingTime !== 0) {
-        const wpm = wordsNum / (startingTime / 60)
+    const { wordsNum, remainingTime } = useContext(UserContext);
+    if(wordsNum !== 0 && remainingTime !== 0) {
+        const wpm = wordsNum / (remainingTime / 60)
         return ( 
             <>wpm: {Math.round(wpm)}</>
          );
